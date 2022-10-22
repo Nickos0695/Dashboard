20c4765ef83e3a6d886409df716056c825e3ae8d
Nicolas CARON <nicolas1.caron@epitech.eu>
2022-10-22 22:18:52 +0200

    Add Zip

---
+++ dashboard/src/components/Zippopotam.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="m-6 block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Zippopotame</h1>
+      <p class="font-normal text-gray-700 dark:text-gray-400">{{zipo}}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default{
+  name:'Zipopotam',
+    data() {
+      return {
+        zipo: []
+      }
+    },
+    mounted () {
+      axios
+        .get('https://api.zippopotam.us/FR/06000')
+        .then(response => (this.zipo = response.data))
+    },
+  }
+  </script>
--- dashboard/src/views/HomeView.vue
+++ dashboard/src/views/HomeView.vue
@@ -3,6 +3,7 @@
 <div class="grid-cols-3	">
   <ChuckNorris/>
   <Maps/>
+  <Zippopotam/>
 </div>
 </template>
 
@@ -11,13 +12,15 @@
 import Header from '@/components/Header.vue';
 import ChuckNorris from '@/components/ChuckNorris.vue';
 import Maps from '../components/Maps.vue';
+import Zippopotam from '@/components/Zippopotam.vue';
 
 export default {
   name: 'HomeView',
   components: {
     Header,
     ChuckNorris,
-    Maps
+    Maps,
+    Zippopotam
 }
 }
 </script>
